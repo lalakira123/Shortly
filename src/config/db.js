a4a712f60cc61e76c8ceb4f15578b7477eb51ea5
Laerte <laerteakira@gmail.com>
2022-06-08 10:28:01 -0300

    feat: connection to database aws

--- src/config/db.js
+++ src/config/db.js
@@ -4,19 +4,11 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const { Pool } = pg;
-
-const user = 'postgres';
-const password = process.env.PASSWORD_PG;
-const host = 'localhost';
-const port = 5432;
-const database = 'shortly';
-
 const connection = new Pool({
-  user,
-  password,
-  host,
-  port,
-  database
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
 
 export default connection;
