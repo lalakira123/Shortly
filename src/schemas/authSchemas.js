605ec1cec218bdd39c4ce0e2ef9b4380f9311080
Laerte <laerteakira@gmail.com>
2022-06-07 21:19:29 -0300

    fix: Change proprieties of email on Schemas

--- src/schemas/authSchemas.js
+++ src/schemas/authSchemas.js
@@ -2,12 +2,12 @@ import joi from 'joi';
 
 export const signUpSchema = joi.object({
     name: joi.string().trim().required(),
-    email: joi.string().trim().required(),
+    email: joi.string().trim().email().required(),
     password: joi.string().trim().required(),
     confirmPassword: joi.string().valid(joi.ref('password'))
 });
 
 export const signInSchema = joi.object({
-    email: joi.string().trim().required(),
+    email: joi.string().trim().email().required(),
     password: joi.string().trim().required()
 });
